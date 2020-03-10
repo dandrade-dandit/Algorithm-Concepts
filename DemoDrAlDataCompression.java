7208232f85d9d90d630b85e9d899ac709d4c266b
denisan2875 <33a653847d7c63919d169321eec4a0ae6806ddad>
2020-03-10 15:30:45 -0700

    Commit 01

--- DemoDrAlDataCompression.java
+++ DemoDrAlDataCompression.java
@@ -1,6 +1,9 @@
 public class DemoDrAlDataCompression {
     public static void main(String[] args) {
         //Insert Code
+
         System.out.println("Insert the code for this problem!");
+
+        //
     }
 }
