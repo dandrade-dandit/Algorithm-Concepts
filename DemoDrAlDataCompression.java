6045c841e9f2c06e5750947dc1aac1ec8e430100
denisan2875 <dan@DAN.localdomain>
2020-03-09 20:03:31 -0700

    file modified

--- DemoDrAlDataCompression.java
+++ DemoDrAlDataCompression.java
@@ -1,5 +1,6 @@
 public class DemoDrAlDataCompression {
     public static void main(String[] args) {
+        //Insert Code
         System.out.println("Insert the code for this problem!");
     }
 }
